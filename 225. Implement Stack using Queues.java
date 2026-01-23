801c74f465f392120148adb3aa2b051ba89f308b
Pushpitha <pushpithagowda776@gmail.com>
2026-01-23 21:47:59 +0530

    225. Implement Stack using Queues

--- 225. Implement Stack using Queues.java
+++ 225. Implement Stack using Queues.java
@@ -38,12 +38,3 @@ class MyStack {
         return queue.isEmpty();
     }
 }
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
