c26cebd9f76f32eee8b884af9b2c6da99abc4063
Pushpitha <pushpithagowda776@gmail.com>
2026-02-07 08:54:15 +0530

    144. Binary Tree Preorder Traversal

---
+++ 144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preorderTraversalHelper(root, res);
+        return res;
+    }
+
+    public void preorderTraversalHelper(TreeNode root, List<Integer> res) {
+        Stack<TreeNode> stack = new Stack<>();
+
+        stack.push(root);
+
+        while(!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            if(node != null) {
+               res.add(node.val);
+               if(node.right != null) {
+                stack.push(node.right);
+                }
+                if(node.left != null) {
+                    stack.push(node.left);
+                }
+            }
+        }
+    }
+}
