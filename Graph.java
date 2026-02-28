d56f046f6d76b3f69ddd877ba1277364daba71c3
Pushpitha <pushpithagowda776@gmail.com>
2026-02-28 20:06:39 +0530

    graph implementation

--- Graph.java
+++ Graph.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Graph {
     int[][] matrix;
+    List<List<Integer>> list;
     public Graph(int nodes) {
         matrix = new int[nodes][nodes];
+        list = new ArrayList<>();
+        for(int i=0; i<nodes; i++) {
+            list.add(new ArrayList<>());
+        }
     }
 
     public void adjMatrix(int[][] edges, boolean isDirected) {
@@ -20,6 +27,26 @@ public class Graph {
         }
     }
 
+    public void adjList(int[][] edges, boolean isDirected) {
+        for(int i=0; i<edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            if(isDirected) {
+                list.get(u).add(v);
+            } else {
+                list.get(u).add(v);
+                list.get(v).add(u);
+            }
+        }
+    }
+
+    public void printList() {
+        for(int i=0; i<list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
     public void printMatrix() {
         for(int i=0; i<matrix.length; i++) {
             System.out.println(Arrays.toString(matrix[i]));
@@ -31,5 +58,7 @@ public class Graph {
         int[][] edges = {{0,2}, {0,1}, {1,3}};
         graph.adjMatrix(edges, false);
         graph.printMatrix();
+        graph.adjList(edges, true);
+        graph.printList();
     }
 }
