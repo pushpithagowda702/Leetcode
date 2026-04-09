6228467461ebc70d219e9046c18d661bd66592a4
Pushpitha <pushpithagowda776@gmail.com>
2026-04-09 19:12:01 +0530

    41. First Missing Positive

---
+++ 41. First Missing Positive.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+
+        boolean oneExists = false;
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] <= 0 || nums[i] > n) {
+                nums[i] = n + 1;
+            }
+
+            if(nums[i] == 1) {
+                oneExists = true;
+            }
+        }
+        if(!oneExists) return 1;
+
+        for(int i=0; i<n; i++) {
+            int ind = Math.abs(nums[i]);
+            if(ind <= n) {
+                nums[ind - 1] = -Math.abs(nums[ind - 1]);
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] > 0) return i + 1;
+        }
+
+        return n + 1;
+    }
+}
