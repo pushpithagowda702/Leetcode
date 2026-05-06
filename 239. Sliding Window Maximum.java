a3d1e16045926a9c9b8cf264b3f45f233a341ae2
Pushpitha <pushpithagowda776@gmail.com>
2026-05-06 18:20:15 +0530

    239. Sliding Window Maximum

--- 239. Sliding Window Maximum.java
+++ 239. Sliding Window Maximum.java
@@ -1,17 +1,24 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int i = 0, j = 0;
+        int j = 0, i = 0;
         int n = nums.length;
         int[] sol = new int[n - k + 1];
-        Arrays.fill(sol, Integer.MIN_VALUE);
-        while(j < k) {
-            sol[i] = Math.max(sol[0], nums[j]);
-            j++;
-        }
-
+        Deque<Integer> que = new ArrayDeque<>();
+        
         while(j < n) {
-            sol[i+1] = Math.max(sol[i], nums[j]);
-            i++;
+            while(!que.isEmpty() && nums[que.peekLast()] < nums[j]) {
+                que.pollLast();
+            }
+
+            que.offer(j);
+
+            if (que.peekFirst() <= j - k) {
+                que.pollFirst();
+            }
+            
+            if (j >= k - 1) {
+                sol[i++] = nums[que.peekFirst()];
+            }
             j++;
         }
         return sol;
