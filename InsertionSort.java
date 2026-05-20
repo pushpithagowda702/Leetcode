7b03694ea98861c2d9f8adf32d8f39e4851042a9
Pushpitha <pushpithagowda776@gmail.com>
2026-05-20 21:42:08 +0530

    Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = new int[] {23, 1, 10, 5, 2};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void insertionSort(int[] arr) {
+        int i = 0, j = i + 1;
+
+        while(j < arr.length) {
+            while(arr[i] < arr[j]) {
+                i++;
+            }
+        }
+    }
+
+    private static void swap(int a, int b, int[] arr) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
