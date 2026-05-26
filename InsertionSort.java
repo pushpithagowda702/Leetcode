76a06dc7ddfce9fb67a9ec34abf8b711e8347f44
Pushpitha <pushpithagowda776@gmail.com>
2026-05-26 20:26:15 +0530

    Insertion sort to be updated

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,25 +1,26 @@
 import java.util.Arrays;
 
-class InsertionSort {
-    public static void main(String[] args) {
-        int[] arr = new int[] {23, 1, 10, 5, 2};
-        insertionSort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
+// To be updated
+// class InsertionSort {
+//     public static void main(String[] args) {
+//         int[] arr = new int[] {23, 1, 10, 5, 2};
+//         insertionSort(arr);
+//         System.out.println(Arrays.toString(arr));
+//     }
 
-    public static void insertionSort(int[] arr) {
-        int i = 0, j = i + 1;
+//     public static void insertionSort(int[] arr) {
+//         int i = 0, j = i + 1;
 
-        while(j < arr.length) {
-            while(arr[i] < arr[j]) {
-                i++;
-            }
-        }
-    }
+//         while(j < arr.length) {
+//             while(arr[i] < arr[j]) {
+//                 i++;
+//             }
+//         }
+//     }
 
-    private static void swap(int a, int b, int[] arr) {
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
-    }
-}
+//     private static void swap(int a, int b, int[] arr) {
+//         int temp = arr[a];
+//         arr[a] = arr[b];
+//         arr[b] = temp;
+//     }
+// }
