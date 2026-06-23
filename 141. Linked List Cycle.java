b308cdd474f63f242a852c3278b0172b41151edf
Pushpitha <pushpithagowda776@gmail.com>
2026-06-23 22:27:35 +0530

    141. Linked List Cycle

--- 141. Linked List Cycle.java
+++ 141. Linked List Cycle.java
@@ -1,9 +1,7 @@
 public class Solution {
     public boolean hasCycle(ListNode fast) {
         if(fast == null || fast.next == null) return false;
-
         ListNode slow = fast;
-
         while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
