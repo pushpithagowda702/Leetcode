4e71e94a5f7ce3995d292c72ada35f19011eaeef
Pushpitha <pushpithagowda776@gmail.com>
2026-07-04 21:27:34 +0530

    11. Container With Most Water

--- 11. Container With Most Water.java
+++ 11. Container With Most Water.java
@@ -1,13 +1,17 @@
 class Solution {
     public int maxArea(int[] height) {
         int i = 0, j = height.length - 1;
-        int max = 0;
-
+        int count = 0;
         while(i < j) {
-            max = Math.max(max, (j - i) * Math.min(height[i], height[j]));
-            if(height[i] > height[j]) j--;
-            else i++;
+            int b = j - i;
+            int l = Math.min(height[i], height[j]);
+            count = Math.max(count, l*b);
+            if(height[i] > height[j]) {
+                j--;
+            }  else {
+                i++;
+            }
         }
-        return max;
+        return count;
     }
 }
