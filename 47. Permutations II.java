d5225c6217da2bd73339782254a775002034cfe6
Pushpitha <pushpithagowda776@gmail.com>
2026-07-06 21:55:46 +0530

    47. Permutations II

--- 47. Permutations II.java
+++ 47. Permutations II.java
@@ -13,7 +13,6 @@ class Solution {
             res.add(new ArrayList<>(curr));
             return;
         }
-
         for(int i=0; i<nums.length; i++) {
             if(track[i] || (i > 0 && nums[i] == nums[i - 1] && !track[i - 1])) continue;
             curr.add(nums[i]);
