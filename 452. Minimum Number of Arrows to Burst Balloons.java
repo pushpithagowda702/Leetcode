7bd0553455d30d8b9079b59892fb098bbb0929b7
Pushpitha <pushpithagowda776@gmail.com>
2026-07-27 13:52:47 +0530

    452. Minimum Number of Arrows to Burst Balloons

--- 452. Minimum Number of Arrows to Burst Balloons.java
+++ 452. Minimum Number of Arrows to Burst Balloons.java
@@ -1,25 +1,16 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        int n = points.length;
-         Arrays.sort(points, new Comparator<int[]>() {
-            public int compare(int x[], int y[]) {
-                if(x[1] == y[1] || x[1] < y[1]) return -1;
-                else return 1;
-            }
-        });
-
-        int i = 1;
-        int res = 1;
-        int currRangeEnd = points[0][1];
+        Arrays.sort(points, (a,b) -> (a[1]<=b[1]) ? -1 : 1);
         
-        while(i < n) {
-            if(currRangeEnd < points[i][0]) {
-                currRangeEnd = points[i][1];
-                res++;
+        int lastEnd = points[0][1];
+        int count = 1;
+        for(int i=1; i<points.length; i++) {
+            int[] curr = points[i];
+            if(curr[0] > lastEnd) {
+                count++;
+                lastEnd = curr[1];
             }
-            i++;
-            
         }
-        return res;
+        return count;
     }
 }
