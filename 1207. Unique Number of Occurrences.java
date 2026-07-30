792a1c12a0632b5a4279267210be2028ad0c04dc
Pushpitha <pushpithagowda776@gmail.com>
2026-07-30 12:30:26 +0530

    1207. Unique Number of Occurrences

---
+++ 1207. Unique Number of Occurrences.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        return map.size() == new HashSet<>(map.values()).size();
+    }
+}
