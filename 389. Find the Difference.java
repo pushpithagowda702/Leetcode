f3a83fb23aeeee0a034101c5662bca1a201f2e61
Pushpitha <pushpithagowda776@gmail.com>
2026-08-01 14:15:52 +0530

    389. Find the Difference

---
+++ 389. Find the Difference.java
@@ -0,0 +1,11 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char ch = 0;
+        for(int i=0; i<s.length(); i++) {
+            ch ^= s.charAt(i);
+            ch ^= t.charAt(i);
+        }
+        ch ^= t.charAt(t.length()-1);
+        return ch;
+    }
+}
